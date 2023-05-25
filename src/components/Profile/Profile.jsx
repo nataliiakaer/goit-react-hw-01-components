5294a66e1db85f893cd166e0569e8b892e59013e
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2023-05-25 20:58:00 +0300

    First task

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,29 @@
+import './Profile.css';
+
+export const Profile = props => {
+  return (
+    <div className="profile" key="profile">
+      <div className="description">
+        <img src={props.avatar} alt={props.username} className="avatar" />
+        <p className="name">{props.username}</p>
+        <p className="tag">@{props.tag}</p>
+        <p className="location">{props.location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{props.stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{props.stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{props.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};

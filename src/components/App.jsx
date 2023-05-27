76762af9386fcdeb93a9c4a0a63526239bf5fa23
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2023-05-27 06:59:16 +0300

    Second task add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Fragment } from 'react';
 import { Profile } from './Profile/Profile';
 import user from 'path/user';
+import { Statistics } from './Statistics/Statistics';
+import data from 'path/data';
 
 export const App = () => {
   return (
-    <>
+    <Fragment>
       <Profile
         username={user.username}
         tag={user.tag}
@@ -12,6 +15,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-    </>
+      <Statistics title="Upload stats" stats={data} />
+    </Fragment>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,9 @@
+import PropTypes from 'prop-types';
 import './Profile.css';
 
 export const Profile = props => {
   return (
-    <div className="profile" key="profile">
+    <section className="profile">
       <div className="description">
         <img src={props.avatar} alt={props.username} className="avatar" />
         <p className="name">{props.username}</p>
@@ -11,19 +12,27 @@ export const Profile = props => {
       </div>
 
       <ul className="stats">
-        <li>
+        <li className="stats-item">
           <span className="label">Followers</span>
           <span className="quantity">{props.stats.followers}</span>
         </li>
-        <li>
+        <li className="stats-item">
           <span className="label">Views</span>
           <span className="quantity">{props.stats.views}</span>
         </li>
-        <li>
+        <li className="stats-item">
           <span className="label">Likes</span>
           <span className="quantity">{props.stats.likes}</span>
         </li>
       </ul>
-    </div>
+    </section>
   );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.objectOf(PropTypes.number),
 };
